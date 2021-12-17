3a4afca77cd2226a6f3b226af6e0da35b48debb6
PolinaShatova <polinashatova4@yandex.ru>
2021-12-17 16:14:12 +0500

    timer

--- DateCalculator/js/utils/timer.js
+++ DateCalculator/js/utils/timer.js
@@ -1,3 +1,26 @@
 import { Howl } from "../libs/howler";
 
 const formTimer = document.getElementById("timer");
+const btnStart = document.getElementById('btnStart');
+const btnStop = document.getElementById('btnStop');
+let timeShow = document.getElementsById('timeShow');
+
+btnStart.addEventListener('click', function () {
+   time = parseInt(formTimer.value) * 60
+})
+
+timer = setInterval(function () {
+    seconds = time % 60;
+    minutes = time / 60 % 60;
+    hour = time / 60 / 60 % 60;
+
+    if (time <= 0) {
+        clearInterval(timer);
+        const timeEnd = `Время закончилось`;
+        timeShow.innerHTML = timeEnd;
+    } else {
+        let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
+        timeShow.innerHTML = strTimer;
+    }
+    --time;
+},1000) 
